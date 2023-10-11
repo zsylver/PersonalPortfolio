e4702f07603e6b09e0692b72e62533380557a197
sylver <sisteneff@gmail.com>
2023-10-12 01:08:51 +0800

    Added Logo + Sidebar

---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import Sidebar from "../Sidebar"
+import './index.scss';
+
+
+const Layout = () => {
+    return (
+        <>
+            <Sidebar />
+        </>
+    )
+}
+
+export default Layout
